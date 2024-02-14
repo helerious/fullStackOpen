7bae6a1306981282e51c8acadc3b0cf759443b04
Helerious <helendong04@gmail.com>
2024-02-14 22:20:52 +1100

    1.3 done

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -6,12 +6,12 @@ const Header = (props) =>{
   )
 }
 const Content = (props) =>{
-  //console.log("content props is", props)
+  console.log("content props is", props)
   return (
     <div>
-      <Part part = {props.part1} and exercise = {props.exercise1}/>
-      <Part part = {props.part2} and exercise = {props.exercise2}/>
-      <Part part = {props.part3} and exercise = {props.exercise3}/>
+      <p>{props.part1} {props.exercise1}</p>
+      <p>{props.part2} {props.exercise2}</p>
+      <p>{props.part3} {props.exercise3}</p>
     </div>
   )
 }
@@ -22,30 +22,36 @@ const Total = (props) =>{
     </div>
   )
 }
-const Part = (props) =>{
+/*const Part = (props) =>{
   //console.log("part props is", props)
   return (
     <div>
       <p>{props.part} {props.exercise}</p>
     </div>
   )
-}
+}*/
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course = {course} />
-      <Content part1 = {part1} and exercise1 = {exercises1} />
-      <Content part2 = {part2} and exercise2 = {exercises2} />
-      <Content part3 = {part3} and exercise3 = {exercises3} />
-      <Total count1 = {exercises1} count2 = {exercises2} count3 = {exercises3}/>
+      <Content part1 = {part1.name} and exercise1 = {part1.exercises} />
+      <Content part2 = {part2.name} and exercise2 = {part2.exercises} />
+      <Content part3 = {part3.name} and exercise3 = {part3.exercises} />
+      <Total count1 = {part1.exercises} count2 = {part2.exercises} count3 = {part3.exercises}/>
     </div>
   )
 }
